b38e620e99d4896df45492ac89f1633521315be0
Ritabrata Ghosh <76sonali40@gmail.com>
2026-05-29 03:08:57 +0530

    init setup done

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-3xl font-bold">KoinX Tax Loss Harvesting</h1>
+    </div>
+  );
+}
+
+export default App;
